d98c9f98b9f885fce7db0bfa6551d659ee64d40f
Brendan Lesniak <brednan.lesniak@gmail.com>
2015-10-07 00:16:46 -0500

    Made progress on String priting, halfway done with tutorial. Once some more rudimentary progress is made on screen prting, I will try to setup some stack space for myself, and see if I can lear about existing devices and file systems. Then the goal is to ready ourselves for real mode.

--- Phase-2-VideoModes-A20-Printing/boot_print.asm
+++ Phase-2-VideoModes-A20-Printing/boot_print.asm
@@ -1,7 +1,8 @@
+;==================================================
 ; The following lines of code are interpreted by the
 ; compiler, and are not a part of the output binary.
 ; The directives are specifed below in comments.
-
+;==================================================
 [BITS 16]			; When loaded by the operating system, we are
 				; loaded into 16 bit mode. Thus, the kernel
 				; bootloader needs to be 16 bits.
@@ -9,23 +10,65 @@
 [ORG 0x7C00]			; We get loaded into memory at location 0x7C00
 				; by BIOS
 
+jmp start			; Safely jump ourselves away from any stored
+				; data in the data segement.
+;==================================================
+; 		DATA SEGEMENT
+;==================================================
+warmupmsg db "Press any key to continue booting...", 10, 13, 0
+bootmsg db "Hello BIOS! Thanks!", 0
+;==================================================
+;		CODE SEGMENT
+;=================================================
 start:
-	xor eax, eax		; 0 out eax to clear junk
-	mov eax, 0xBEAD0501	; This is a signature for BrenOS
+	xor ax, ax		; 0 out eax to clear junk
+	mov ds, ax		; Set the current data segment offset to 0
+	mov es, ax		; Do the same with es segement register
 
-	; Here we are going to try and call a BIOS interrupt to
-	; print some characters to the screen.
-	; I need to setup some registers to do this correctly:
-	; ah = function to call when interrupt handler is called,
-	; al = Character, bl = Color
-	mov ah, 0x0E
-	mov bl, 0x40
-	mov al, 'H'
+	call clear_screen	; Clear the screen before we try to print
+				; any strings to the screen
+
+	mov si, warmupmsg
+	call write_string
+	call wait_for_input
+
+
+	mov si, bootmsg		; Move the stack data pointer to point to
+				; our bootmsg and call the print routines
+	call write_string
+
+	jmp loop
+
+clear_screen:
+	mov ah, 0
 	int 0x10
+	ret
 
-	jmp .loop
-.loop:
-	jmp .loop		; Infinite loop when this is called, nothing
+wait_for_input:
+	xor ax, ax
+	int 0x16
+	ret
+
+write_string:
+	lodsb			; Load the string buffer at ds:si
+	or al, al		; or the current character to ...
+	jz .write_string_end	; If it is 0 (null terminator) jump to the
+				; end print function. Else continue.
+	call write_character	; Write the character that is in the buffer
+	jmp write_string	; Do this until the buffer end is reached
+
+.write_string_end:
+	ret
+
+write_character:
+	mov bl, 0x04		; Don't make any register assumptions, always
+				; set to wanted color here.
+	mov ah, 0x0E
+	int 0x10
+	ret
+
+loop:
+	jmp loop		; Infinite loop when this is called, nothing
 				; else to do.
 	;cli			; Clear all of the interrupts that were generated
 	;hlt			; Halt any system execution
