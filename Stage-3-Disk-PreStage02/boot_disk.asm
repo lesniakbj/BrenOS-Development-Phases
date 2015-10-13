af20b9bb4ae1830a5860c9ad533783c880df1330
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-10-12 20:26:05 -0500

    Added in comments, and formatting to code

--- Stage-3-Disk-PreStage02/boot_disk.asm
+++ Stage-3-Disk-PreStage02/boot_disk.asm
@@ -17,25 +17,25 @@ jmp bootloader_start		; Safely jump ourselves away from any stored
 ; 		DATA SEGEMENT
 ;==================================================
 ; OEM Parameter Block
-oemName 	db "BrenOS  "	; Must be 8 Bytes long, thus padded with spaces
+oemName 		db "BrenOS  "	; Must be 8 Bytes long, thus padded with spaces
 bytesPerSector:	dw 512
 sectorsPerClus: db 1
 reservedSectrs: db 1
 numberOfFATs:	db 2
 rootEntries:	db 224
 totalSectors:	dw 2880
-media:		db 0xF0
+media:			db 0xF0
 sectorsPerFAT:	dw 9
 sectorsPerTrk:	dw 18
 headsPerCyln:	dw 2
 hiddenSectors:	dd 0
 totalSectBig:	dd 0
-driveNum:	db 0
-unused:		db 0
-extBootSig:	db 0x29
-serialNum:	dd 0xa0a1a2a3
-volLabel:	db "BOS FLOPPY "
-fileSystem:	db "FAT12   "
+driveNum:		db 0
+unused:			db 0
+extBootSig:		db 0x29
+serialNum:		dd 0xa0a1a2a3
+volLabel:		db "BOS FLOPPY "
+fileSystem:		db "FAT12   "
 
 ; Bootloader Static Messages / Data
 newline 	db 0x0A, 0x0D, 0
@@ -47,7 +47,7 @@ dskendmsg	db "Boot02 has been read..", 0x0A, 0x0D, 0
 cntrlmsg	db "Handing off control to Boot02..", 0x0A, 0x0D, 0
 
 ; Bootloader datq output swap space
-hex_16_out: 	db '0x0000', 0
+hex_16_out: db '0x0000', 0
 disk_count	db 0
 
 ;==================================================
@@ -70,12 +70,12 @@ bootloader_start:
 	; to use. This will be used for function calls, and getting ready
 	; for our stage02 bootloader. 
 	mov ax, 0x9E00		; Set up 4K of stack space after this bootloader
-				; code. Start with where this code is loaded
-				; from. 
-	mov ss, ax		; Point our SS to the segment directly after
-				; the bootloader
+						; code. Start with where this code is loaded
+						; from. 
+	mov ss, ax			; Point our SS to the segment directly after
+						; the bootloader
 	mov sp, 4096		; Move our stack pointer to SS:4096, giving us
-				; 4K of stack space to work with.
+						; 4K of stack space to work with.
 	sti
 
 	
@@ -85,11 +85,15 @@ bootloader_start:
 	call set_screen_mode
 
 	call clear_screen	; Clear the screen before we try to print
-				; any strings to the screen
+						; any strings to the screen
 
+						
+	;-------------------------
+	; STACK SETUP INFORMATION
+	;-------------------------
 	mov si, stackmsg
 	call write_string
-	mov ax, ss		; Move SS into AX for printing
+	mov ax, ss			; Move SS into AX for printing
 	mov dx, ax
 	call print_hex
 	
@@ -108,6 +112,9 @@ bootloader_start:
 	mov si, newline
 	call write_string
 	
+	;-------------------------
+	; DISK SECTION
+	;-------------------------
 	; Now that we have detected our Low Memory, we want to do some
 	; setup so that we can read our 2nd stage bootloader from
 	; the rest of the drive. 
@@ -119,9 +126,9 @@ bootloader_start:
 	; Frankly, on second thought, at this level there is no idea of "reserved"
 	; space. Rather, I have a slot in memory that I am given to use. 
 	; Hopefully we don't overflow...
-	mov ax, 0x7E00		; AX = Address where we are going to read a sector
-				; into. This is the beginning of the disk buffer, 
-				; the first bytes beyond the bootloader.
+	mov ax, 0x7E00	; AX = Address where we are going to read a sector
+					; into. This is the beginning of the disk buffer, 
+					; the first bytes beyond the bootloader.
 	mov es, ax		; ES:BX = The where the sectors will be read to
 	xor bx, bx		; 0x7E00:0x0000 -> ES:0
 	
@@ -130,8 +137,8 @@ bootloader_start:
 	
 	; The disk has been read, lets make sure we we read the
 	; correct number of sectors
-	cmp al, 1
-	jne disk_error
+	; cmp al, 1
+	; jne disk_error
 	
 	; Now that we have read that sector to the disk, we can jump to it and
 	; continue execution! Unfortunately, this will not quite work yet, 
@@ -163,89 +170,87 @@ clear_screen:
 	ret
 
 write_string:
-	lodsb			; Load the string buffer at ds:si
-	or al, al		; or the current character to ...
+	lodsb					; Load the string buffer at ds:si
+	or al, al				; or the current character to ...
 	jz .write_string_end	; If it is 0 (null terminator) jump to the
-				; end print function. Else continue.
+							; end print function. Else continue.
 	call write_character	; Write the character that is in the buffer
-	jmp write_string	; Do this until the buffer end is reached
+	jmp write_string		; Do this until the buffer end is reached
 
 .write_string_end:
 	ret
 
 write_character:
 	mov bl, 0x04		; Don't make any register assumptions, always
-				; set to wanted color here.
+						; set to wanted color here.
 	mov ah, 0x0E
 	int 0x10
 	ret
 
 print_hex:
-	;pusha			; Once stack is setup, push regs before
-				; calling so state can be restored
-
-	mov cx, 4		; Start the counter. AX contains 4 "characters"
+	mov cx, 4	; Start the counter. AX contains 4 "characters"
 				; and thus our counter is 4.  4 bits per char.
 				; Control continues to hex_to_char_loop
+				
 .hex_to_char_loop:
-	dec cx			; Pre decrement our counter
+	dec cx				; Pre decrement our counter
 
-	mov ax, dx		; Copy DX to BX so we can mask it
-	shr dx, 4		; Shift it 4 bits to the right, so we are 
-				; dealing witha  value such:
-				; 0xF29A  -->  0x00F2
+	mov ax, dx			; Copy DX to BX so we can mask it
+	shr dx, 4			; Shift it 4 bits to the right, so we are 
+						; dealing witha  value such:
+						; 0xF29A  -->  0x00F2
 	and ax, 0x0F		; Get the last 4 bits, one character
 
 	mov bx, hex_16_out	; Set BX to our output memory address
-	add bx, 2		; Skip the '0x' char in the string
-	add bx, cx		; Add the counter to the address so
-				; we can address the correct byte in the string
+	add bx, 2			; Skip the '0x' char in the string
+	add bx, cx			; Add the counter to the address so
+						; we can address the correct byte in the string
 
-	cmp ax, 0xA		; Check to see if this is a letter or number
+	cmp ax, 0xA			; Check to see if this is a letter or number
 	jl .set_letter		; If its a number, set the value immediately
-				; otherwise there is some preprocessing
+						; otherwise there is some preprocessing
 	add byte [bx], 7	; If its a letter, add 7 to offset to ASCII
-				; values
+						; values
 
 	jl .set_letter
 
 .set_letter:
-	add byte [bx], al	; Add the value of the byte to the char at bx
+	add byte [bx], al		; Add the value of the byte to the char at bx
 
-	cmp cx, 0		; Check the counter to see if we are done
-	je .print_hex_done	; If we are done, head on out of here
+	cmp cx, 0				; Check the counter to see if we are done
+	je .print_hex_done		; If we are done, head on out of here
 	jmp .hex_to_char_loop	; Otherwise, leads head back to the printing
-				; loop so we can print the value
+							; loop so we can print the value
 
 .print_hex_done:
 	mov si, hex_16_out	; Now that we are done converting to char,
-				; lets print that out
+						; lets print that out
 	call write_string
 	ret
 
 reset_disk:
 	mov ah, 0x00		; Move 0 into AH, the function we want to call
-				; 0 = reset floppy function
+						; 0 = reset floppy function
 	mov dl, 0x00		; dl = drive number, 0 the current drive
-	int 0x13		; Call BIOS reset function
+	int 0x13			; Call BIOS reset function
 	jc reset_disk		; If the carry was set there was an error
-				; resetting the disk, try again.
+						; resetting the disk, try again.
 	ret
 	
 read_disk:
 	dec byte [disk_count]
 	mov ah, 0x02		; Function 0x02 = Read Disk Sector
-	mov al, 1		; AL = # of sectors to read, we want the first sector
-	mov ch, 1		; CH = Track number to read from, we are on the
-				; 1st track, along with the data
-	mov cl, 2		; CL = Sector to Read, we want the second sector (passed 
-				; the bootloader code)
-	mov dh, 0		; DH = Drive Head Number, the 0th head
-	mov dl, 0		; DL = Drive Number, 0th drive is the floppy drive
-	int 0x13		; BIOS call to read the sector based on the params
-				; setup in the previous block
+	mov al, 1			; AL = # of sectors to read, we want the first sector
+	mov ch, 1			; CH = Track number to read from, we are on the
+						; 1st track, along with the data
+	mov cl, 2			; CL = Sector to Read, we want the second sector (passed 
+						; the bootloader code)
+	mov dh, 0			; DH = Drive Head Number, the 0th head
+	mov dl, 0			; DL = Drive Number, 0th drive is the floppy drive
+	int 0x13			; BIOS call to read the sector based on the params
+						; setup in the previous block
 	cmp byte [disk_count], 0
-	jne read_disk		; If there is an error, try again
+	jne read_disk		; If there is an error, and we haven't tried 5 times, try again
 	
 	ret
 
