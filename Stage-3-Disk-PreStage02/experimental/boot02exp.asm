032bbd4f2c24da5b21f15462479dff76dcc7a122
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-10-13 21:30:10 -0500

    Fixed string. Added first attempt at 2 file 2 stage loader.

---
+++ Stage-3-Disk-PreStage02/experimental/boot02exp.asm
@@ -0,0 +1,12 @@
+BOOT2_SAMEFILE_MSG:	db "Has this been read from disk yet?!?", 0
+
+; NOTE:
+; ======================
+; Some emulators and disk drives will
+; not read a sector unless it is fully 
+; padded out, thus we need to pad this
+; sector or it will not be read. This is
+; true of all sectors we read in some 
+; emulators. Thus, the last sector of every
+; code segment must be padded.
+TIMES 512 - ($ - $$) db 0
