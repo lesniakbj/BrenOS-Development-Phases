057cd2b55ab754245b0be4d7e6f380d76393b423
Brendan Lesniak <brednan.lesniak@gmail.com>
2015-10-13 22:00:11 -0500

    Commit

--- Stage-3-Disk-PreStage02/experimental/boot01exp.asm
+++ Stage-3-Disk-PreStage02/experimental/boot01exp.asm
@@ -17,7 +17,7 @@ read_from_disk:
 	mov ah, 0x02	; Read Sector Function
 	
 	mov al, 1		; Number of Sectors to Read
-	mov dl, 0		; Use the 1st (A:) Drive. Floppy.
+	; mov dl, 0		; Use the 1st (A:) Drive. Floppy.
 	mov ch, 0		; Use the 1st Cylinder/Track
 	mov dh, 0		; Use the 1st Read/Write Head
 	mov cl, 2		; Read the 2nd Sector
