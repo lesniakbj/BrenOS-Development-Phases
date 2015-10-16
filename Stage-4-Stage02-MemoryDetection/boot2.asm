b3510a1809fe349463c8b9b310049467660fe42b
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-10-15 20:45:52 -0500

    Changes to boot1

--- Stage-4-Stage02-MemoryDetection/boot2.asm
+++ Stage-4-Stage02-MemoryDetection/boot2.asm
@@ -1,8 +1,31 @@
 [ORG 0x0000:0x7E00]
 
+mov si, TEST_STRING
+call write_string
+
 mov ax, 0xBE01
 cli
 hlt
+
+write_string:
+	push ax
+	push si
+	
+.string_loop:
+	lodsb
+	cmp al, 0
+	je .string_end
+		
+	mov ah, 0x0E
+	int 0x10
+	jmp .string_loop
+	
+.string_end:
+	pop si
+	pop ax
+	ret
+
+TEST_STRING db 'Are we loaded correctly?!'
 
 ; NOTE:
 ; ======================
