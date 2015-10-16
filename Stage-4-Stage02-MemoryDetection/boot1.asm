43f0c99935e557570cfc3ade44ced16ab002490d
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-10-15 20:42:18 -0500

    Changes to boot1

--- Stage-4-Stage02-MemoryDetection/boot1.asm
+++ Stage-4-Stage02-MemoryDetection/boot1.asm
@@ -115,6 +115,60 @@ read_from_disk:
 	mov si, [READ_ERROR]
 	; call write_string
 	jmp $
+	
+
+write_string:
+	push ax
+	push si
+	
+.string_loop:
+	lodsb
+	cmp al, 0
+	je .string_end
+		
+	mov ah, 0x0E
+	int 0x10
+	jmp .string_loop
+	
+.string_end:
+	pop si
+	pop ax
+	ret
+
+	
+write_hex:
+	push bx
+	push si
+	
+	mov bx, dx
+	shr bx, 12
+	and bx, 0x0F
+	add bx, HEX_CHARS
+	mov bl, [bx]
+	mov [HEX_OUT + 2], bl
+	
+	mov bx, dx
+	shr bx, 8
+	and bx, 0x0F
+	add bx, HEX_CHARS
+	mov bl, [bx]
+	mov [HEX_OUT + 3], bl
+	
+	mov bx, dx
+	shr bx, 4
+	and bx, 0x0F
+	add bx, HEX_CHARS
+	mov bl, [bx]
+	mov [HEX_OUT + 4], bl
+	
+	mov bx, dx
+	and bx, 0x0F
+	add bx, HEX_CHARS
+	mov bl, [bx]
+	mov [HEX_OUT + 5], bl
+	
+	mov si, HEX_OUT
+	call write_string
 
 ;===================;
 ;	BOOT-1 DATA
@@ -123,6 +177,8 @@ read_from_disk:
 READ_ERROR: db "Error reading disk!", 0
 	
 ; Other Data
+HEX_CHARS: db '0123456789ABCDEF'
+HEX_OUT: db '0x???? ', 0
 diskNumber	db 0
 	
 TIMES 510 - ($ - $$) db 0 
