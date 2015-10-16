b508cb8bc7535489526d28ed1e78fbd8b1e2acbf
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-10-15 22:32:34 -0500

    Changes to boot1

--- Stage-4-Stage02-MemoryDetection/boot1.asm
+++ Stage-4-Stage02-MemoryDetection/boot1.asm
@@ -219,3 +219,41 @@ dw 0xAA55
 ; Here we create a label to jump to
 ; when Stage02 loads.
 stage02_load:
+
+mov si, TEST_STRING
+call write_string
+
+mov ax, 0xBE01
+cli
+hlt
+
+write_string:
+	push ax
+	push si
+	
+.string_loop:
+	lodsb
+	cmp al, 0
+	je .string_end
+		
+	mov ah, 0x0E
+	int 0x10
+	jmp .string_loop
+	
+.string_end:
+	pop si
+	pop ax
+	ret
+
+TEST_STRING db 'Are we loaded correctly?!'
+
+; NOTE:
+; ======================
+; Some emulators and disk drives will
+; not read a sector unless it is fully 
+; padded out, thus we need to pad this
+; sector or it will not be read. This is
+; true of all sectors we read in some 
+; emulators. Thus, the last sector of every
+; code segment must be padded.
+TIMES 512 db 0
