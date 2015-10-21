b29dd2ff86889200e797c6325c32cff996a2e441
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-10-20 22:31:59 -0500

    Fixed parameter

--- Stage-6-HDDLoad-LBACHS/boot1.asm
+++ Stage-6-HDDLoad-LBACHS/boot1.asm
@@ -67,7 +67,7 @@ boot1_start:
 	; Ok, we are using a floppy dive, lets
 	; save that so it can be used. 
 	mov dword [readFunction], read_sector_fd
-	jmp boot_floppy
+	jmp load_second_stage
 	
 boot_hdd:
 	mov dword [readFunction], read_sector_hdd
@@ -99,7 +99,7 @@ boot_hdd:
 	; blocks for the MBR.
 	inc cx
 	cmp cx, 4
-	je .boot_mbr_not_found
+	je load_second_stage
 	
 	add eax, 16
 	jmp .check_next_sector
@@ -114,14 +114,12 @@ boot_hdd:
 	mov ebx, [eax + 8]
 	mov [diskPartitionLBA], ebx
 
-.boot_mbr_not_found:
+load_second_stage:
 	mov eax, 1
 	mov bx, 0x7E00
 	mov cx, 4
 	call read_sector
 	jmp 0x0000:stage02_load
-	
-boot_floppy:
 	
 	
 %include "funcs/disk_functions.asm"
--- Stage-6-HDDLoad-LBACHS/funcs/disk_functions.asm
+++ Stage-6-HDDLoad-LBACHS/funcs/disk_functions.asm
@@ -1,6 +1,19 @@
 ;=======================;
 ;	PRIMARY FUNCTIONS	;
 ;=======================;
+reset_drive:
+	pusha
+	
+.retry:
+	mov ax, 0
+	mov dl, [diskNumber]
+	int 0x13
+	
+	jc .retry
+	
+	popa
+	ret
+
 read_sector:
 	jmp [readFunction]
 
@@ -24,20 +37,78 @@ read_sector_hdd:
 	mov [transferSeg], es
 	mov [sectorsToGet], cx
 	
-	; Call the int so we can read the struct
+	; Call the int so we can read the struct,
+	; in turn reading the sectors we want from
+	; the drive. 
 	mov ah, 0x42
 	mov si, diskLBAPacket
 	int 0x13
 	
 	a32 popa
 	ret
+
 ;---------------------------------------;
-; read_sector_fd(sector, dest*, length);
+; read_sector_fd(sector, dest*, length)	;
 ;		EAX - Sector to Read			;
 ;		ES:BX - Destination				;
 ;		CX - Length						;
 ;---------------------------------------;
 read_sector_fd:
+	a32 pusha
+	
+.read_next_sector:
+	call read_single_sector_fd
+	
+	; Lets move to the next sector
+	; to read from.
+	add bx, 512
+	inc eax
+	
+	; Loop will decrement CX on every
+	; iteration so we do not need to do that.
+	loop .read_next_sector
+	
+.done:
+	a32 popa
+	ret
+	
+read_single_sector_fd:
+	a32 pusha
+	
+	; FD's need CHS values instead of
+	; LBA. Lets do the conversion.
+	call get_chs_values
+	
+.read_sector:
+	call reset_drive
+	mov dl, [diskNumber]
+	; Read/# of Sectors combined
+	mov ax, 0x0201
+	int 0x13
+	
+	jc .read_sector
+	
+	a32 popa
+	ret
+	
+;---------------------------------------;
+; get_chs_values(sector, dest*, length)	;
+;		EAX - Sector to Read			;
+;		ES:BX - Destination				;
+;		CX - Length						;
+;---------------------------------------;
+get_chs_values:
+   xor edx, edx                     ;Zero out edx
+   div word [bpbSectorsPerTrack]
+   mov cl, dl                     
+   inc cl
+   xor edx, edx
+   div word [bpbNumberOfHeads]
+   mov dh, dl                     ;Mov dl into dh (dh=head)
+   mov ch, al                     ;Mov cylinder into ch
+   shl ah, 6
+   or  cl, ah
+   ret
 	
 ;=======================;
 ;		DISK DATA		;
