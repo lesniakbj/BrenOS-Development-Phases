afc844587735e0e0f70dab79e73355d15e75b81f
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-10-20 20:12:23 -0500

    Clean and started HDD bootloader support

---
+++ Stage-6-HDDLoad-LBACHS/boot1.asm
@@ -0,0 +1,164 @@
+; NOTE:
+;	This file follows the BPB format of
+;	the FAT12 specification, listed at
+;	http://wiki.osdev.org/FAT#BPB_.28BIOS_Parameter_Block.29
+[BITS 16]
+[ORG 0x7C00]
+
+; Cononicalize CS:IP to 0x0000:0x7C00
+; 	Note: Some BIOS' start us at 
+;	0x07C0:0x0000. We fix that.
+jmp short boot1_start
+nop
+
+;====================================;
+;		BIOS PARAMETER BLOCK		 ;
+;====================================;
+oemIdentifier		db 'BrenOS  '
+bytesPerSector		dw 512
+sectorsPerCluster	db 1
+reservedSectors		dw 1
+numberOfFATs		db 1
+rootEntries			dw 224
+totalNumberSectors	dw 0
+mediaDescriptor		db 0xF8
+sectorsPerFAT		dw 0
+sectorsPerTrack		dw 0
+numberOfHeads		dw 0
+hiddenSectors		dd 0
+largeMediaFields	dd 0
+
+;====================================;
+;  FAT12 EXTENDED BOOT RECORD (EBPB) ;
+;====================================;
+driveNumber			db 0x00
+reservedField		db 0x00
+signatureFAT12		db 0x29
+volumeID			dd 0
+volumeLabel			db 'BrenOS Sys '
+fileSystemID		db 'FAT     '
+
+
+; ... whew! Now that we defined all the
+; things "necissary" to define our FAT
+; bootable device, time to start our boot1
+; code.
+
+boot1_start:
+	; Setup the segments
+	cli
+	xor ax, ax
+	mov ds, ax
+	mov es, ax
+	
+	; Time for the stack
+	mov ss, ax
+	mov sp, 0x7C00
+	sti
+	
+	; Save the disk that we are
+	; booted from.
+	mov [diskNumber], dl
+	
+	; DL contains the drive number,
+	; which is conviently where we
+	; put the parameter for the 
+	; write_hex(dl) function.
+	; call write_hex
+	call reset_disk
+	call read_from_disk
+	
+	mov si, BOOT_MSG
+	call write_string
+	
+	xor si, si
+	xor di, di
+	jmp 0x0000:stage02_load
+	jmp $
+	
+; Note: These can't be included due to the
+; fact that they use variables defined here.
+; Thus, they are are simply included as 
+; functions here.
+; %include "funcs/disk_functions.asm"
+; %include "funcs/output_functions.asm"
+
+reset_disk:
+	mov ah, 0				; Reset disk function
+	mov dl, [diskNumber]	; This will only be run if on Floppy
+	int 0x13
+	jc reset_disk
+	
+	ret
+	
+read_from_disk:
+	; Read Sector Function
+	mov ah, 0x02
+	
+	; Setup the function defining where
+	; we are reading from...
+	mov al, 8				; Number of Sectors to Read
+	mov dl, [driveNumber]	; Use the 1st (C:) Drive. HDD.
+	mov ch, 0				; Use the 1st Cylinder/Track
+	mov dh, 0				; Use the 1st Read/Write Head
+	mov cl, 2				; Read the 2nd Sector
+	
+	; Where to buffer the disk read to...
+	; ES:BX -> 0x0000:0x7E00
+	mov bx, 0
+	mov es, bx
+	mov bx, stage02_load
+	
+	; ERROR CHECKING...	
+	mov [readSegment], es
+	mov [readOffset], bx
+	
+	int 0x13
+	
+	jc .disk_read_error
+	ret 
+
+.disk_read_error:
+	mov si, READ_ERROR
+	call write_string
+	jmp $
+	
+
+write_string:
+	push ax
+	push si
+
+	
+.string_loop:
+	lodsb
+	cmp al, 0
+	je .string_end
+		
+	mov ah, 0x0E
+	int 0x10
+	jmp .string_loop
+	
+.string_end:
+	pop si
+	pop ax
+	ret
+
+;========================;
+;		BOOT-1 DATA		 ;
+;========================;
+; String Data
+BOOT_MSG	db 'Loading stage 2 loader...', 0x0A, 0x0D, 0
+
+; Other Data
+diskNumber	db 0
+
+; Error Checking Data
+readSegment	dw 0
+readOffset	dw 0
+	
+TIMES 510 - ($ - $$) db 0 
+dw 0xAA55
+
+; Here we create a label to jump to
+; when Stage02 loads.
+stage02_load:
