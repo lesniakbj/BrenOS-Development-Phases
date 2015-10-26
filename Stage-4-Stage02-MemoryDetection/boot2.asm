bf29b08afb6cb7d7d3668abec56f775158350ae2
Brendan Lesniak <brendan.lesniak@gmail.com>
2015-10-26 18:09:45 -0500

    Added string to now print e820 data

--- Stage-4-Stage02-MemoryDetection/boot2.asm
+++ Stage-4-Stage02-MemoryDetection/boot2.asm
@@ -119,7 +119,7 @@ MEM_DET_MSG			db ' Detecting Memory Map', 0
 LOW_MEM_DET_MSG 	db ' Detecting Low Memory (KB): ', 0
 DIVIDER_MSG				db ' =================================', 0
 HIGH_MEM_MSG 			db ' Number of E820 Entries: ', 0
-MEM_LAYOUT_MSG		db 'Start		End			Type', 0
+MEM_LAYOUT_MSG		db ' Start			End				Type', 0
 BYTES_DET_MSG			db ' Bytes Stored (0x): ', 0
 HIGHMEMERR_MSG		db ' Error Using INT 0x15, AX 0xE820!', 0
 
